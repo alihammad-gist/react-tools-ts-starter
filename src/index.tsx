94a6b15f15bc8fade58b5f98ff778e8656b6b696
alihammad <syedalihammadshah@gmail.com>
2016-01-24 13:36:07 +0500

    fixed styles and module resolution

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,46 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as AppBar from 'react-toolbox/lib/app_bar';
 
-ReactDOM.render(<AppBar></AppBar>, document.getElementById('app'));
+import * as Dropdown from 'react-toolbox/lib/dropdown';
+
+const countries = [
+{ value: 'EN-gb', label: 'England' },
+{ value: 'ES-es', label: 'Spain' },
+{ value: 'TH-th', label: 'Thailand' },
+{ value: 'EN-en', label: 'USA' }
+];
+
+class DropdownTest extends React.Component<{}, {value:string}> {
+	state = {
+		value: 'ES-es',
+	};
+
+	handleChange = (value) => {
+		this.setState({ value: value });
+	};
+
+	render() {
+		return (
+			<Dropdown
+				auto
+				onChange={this.handleChange}
+				source={countries}
+				value={this.state.value}
+				/>
+		);
+	}
+}
+
+class NavBar extends React.Component<{}, {}> {
+	render() {
+		return (
+			<AppBar fixed flat>
+				<DropdownTest />
+			</AppBar>
+		);
+	}
+}
+
+ReactDOM.render(<NavBar />, document.getElementById('app'));
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,35 +7,31 @@ module.exports = {
 		publicPath: "/assets/"
 	},
 	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel', // 'babel-loader' is also a legal name to reference
-				query: {
-					presets: ['react', 'es2015']
-				}
-			},
-			{ 
-				test: /\.tsx?$/, 
-				loader: 'babel-loader?presets[]=react,presets[]=es2015!ts-loader'
-			},
-			{
-				test: /\.scss$/,
-				loaders: ['style', 'css', 'sass']
-			},
-			{
-				test: /\.css$/,
-				loaders: ['style', 'css']
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel', // 'babel-loader' is also a legal name to reference
+			query: {
+				presets: ['react', 'es2015']
 			}
-		]
+		}, {
+			test: /\.tsx?$/,
+			loaders: ['babel?presets[]=react,presets[]=es2015', 'ts']
+		}, {
+			test: /(\.scss|\.css)$/,
+			loaders: ['style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]', 'postcss', 'sass?sourceMap']
+		}]
 	},
 	resolve: {
-		extensions: ['.js', '.jsx', '.css', '.scss', '.ts', '.tsx']
+		extensions: ['', '.js', '.jsx', '.css', '.scss', '.ts', '.tsx', '.json'],
+		moduleDirectories: [
+			__dirname + 'node_modules'
+		]
 	},
 	resolveLoader: {
+		extensions: ['', '.js', '.jsx', '.css', '.scss', '.ts', '.tsx', '.json'],
 		moduleDirectories: [
-			'./node_modules'
+			__dirname + 'node_modules'
 		]
 	}
 }
